70d7fbc4cede90513466ff5d219cc2594614eb38
jnannni <49609088+jnannni@users.noreply.github.com>
2025-05-14 22:40:50 +0200

    added player sprites load

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,5 +1,6 @@
 import { GameField } from "./tetris";
 import { Tetromino } from "./tetris";
+import { Player } from "./player";
 
 const straightTetromino = [[1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
 const squareTetromino = [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
@@ -14,6 +15,7 @@ let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D | null;
 let field: GameField;
 let tetromino: Tetromino;
+let player: Player;
 
 export const startGame = (canvasID: HTMLCanvasElement) => {
     canvas = canvasID;
@@ -22,27 +24,36 @@ export const startGame = (canvasID: HTMLCanvasElement) => {
     canvas.height = innerHeight;
     field = new GameField(480, 560, pxPerCell); 
     tetromino = new Tetromino('red', tTetromino, 417, 40);
+    player = new Player('player', 128, 128, "/shinobi.png");
+
     if (ctx) {
         ctx.fillStyle = 'black';
         ctx.fillRect(277, 20, 984, 600);
-        field.drawField(ctx, 377, 40);                       
+        field.drawField(ctx, 377, 40);                     
     }
-    gameLoop();
+    
+    const waitForSprites = setInterval(() => {
+        if (player.isLoaded) {
+            clearInterval(waitForSprites);
+            requestAnimationFrame(gameLoop);
+        }
+    }, 50);
 }
 
 function gameLoop(timestamp: number = 0) {
     const deltaTime = timestamp - start;
     start = timestamp;
     count += deltaTime;
-    if (count > animTime) {
-        tetromino.moveDown();
-        count = 0;
-    }
+    // if (count > animTime) {
+    //     tetromino.moveDown();
+    //     count = 0;
+    // }
     if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = 'black';
         ctx.fillRect(277, 20, 984, 600);
         field.drawField(ctx, 377, 40);
+        player.drawPlayer(ctx, 0, 0, 0, 0, 128, 128);
         tetromino.draw(ctx);
     }
     requestAnimationFrame(gameLoop);
--- src/game/player.ts
+++ src/game/player.ts
@@ -3,15 +3,28 @@ export class Player {
     height: number;
     width: number;
     playerSprite: HTMLImageElement;
-    constructor(id: string, width: number, height: number, playerSprite: HTMLImageElement) {
+    src: string;
+    isLoaded: boolean = false;
+    constructor(id: string, width: number, height: number, src: string) {
         this.height = height;
         this.width = width;
         this.id = id;
+        this.src = src;
         this.playerSprite = new Image();
-        this.playerSprite.src = "";
+        this.playerSprite.src = src;
+
+        this.playerSprite.onload = () => {
+            this.isLoaded = true;
+        };
+
+        this.playerSprite.onerror = () => {
+            console.error("Error loading player sprite");
+        };
     }
 
-    drawPlayer(ctx: CanvasRenderingContext2D, sX, sY, sW, sH, dX, dY, dW, dH) {
-        ctx.drawImage(this.playerSprite, 0, 0, this.width, this.height);
+    drawPlayer(ctx: CanvasRenderingContext2D, sX: number, sY: number, dX: number, dY: number, dW: number, dH: number): void { 
+        if(this.isLoaded) {
+            ctx.drawImage(this.playerSprite, 0, 0, this.width, this.height, dX, dY, dW, dH);
+        }       
     }
 }
